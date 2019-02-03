ad58e3ac3121d4d7935273fb93a12f68a8e0a535
tyler wood <tylerjaywood@gmail.com>
2019-02-03 14:30:57 -0700

    trying to add the static index html and statics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -8,16 +8,40 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import *
+from secrets import secret_endpoint
+from graphs import SensorGrapher
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    path = os.path.join(root_dir(), 'templates/index.html')
+    return render_template('index.html')
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
+@app.route(secret_endpoint)
+def make_graphs():
+    grapher = SensorGrapher()
+    grapher.fetch_df()
+    grapher.plot_moisture_data()    
+    grapher.plot_temp_data()
+    grapher.plot_light_data()
+    grapher.plot_humid_data()
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ graphs.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+import numpy as np
+import math
+
+from datetime import datetime, timedelta
+from secrets import db_connect
+
+
+class SensorGrapher():
+    def __init__(self):
+        self.cur = db_connect()
+        self.query = """
+        SELECT time_stamp
+            , values_json -> 'temperature_F' as temp
+            , values_json -> 'humidity' as humid
+            , values_json -> 'sunlight' as light 
+            , values_json -> 'top_soil' as top_soil
+            , values_json -> 'mid_soil' as mid_soil
+            , values_json -> 'tray' as tray
+        FROM sensor_json"""
+        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def fetch_df(self):
+        cur.execute(sensor_query)
+        data = cur.fetchall()
+        df = pd.DataFrame(np.array(data))
+        df.columns = ['time_stamp', 'temp', 'humid', 'light', 'top_soil', 'mid_soil', 'tray']
+        df['time_stamp'] = pd.to_datetime(df.time_stamp)
+        df = df.set_index('time_stamp')
+        self.df = df[df.time_stamp >= (datetime.now()-timedelta(days=14))]
+
+    def plot_moisture_data(self):
+        moist_df = self.df[['tray','top_soil', 'mid_soil']]
+        moist_df = moist_df[['top_soil', 'mid_soil', 'tray']].mask(df < 350)
+        fig, axs = plt.subplots(1, 1, figsize=(15,5))
+        axs.plot(moist_df.index, ((moist_df[['top_soil', 'mid_soil', 'tray']]/(-1024))+1))
+        axs.set_title('Moisture Sensors\n timestamp: '+timestamp)
+        axs.set_ylabel('Soil Moisture Readings \n 1 is completely soaked')
+        axs.legend(moist_df.columns)
+        axs.grid(True)
+        plt.savefig('static/moisture.png')
+
+    def plot_temp_data(self):
+        pdf = self.df[['temp']]
+        fig, axs = plt.subplots(1, 1, figsize=(15,5))
+        axs.plot(pdf, color='red')
+        axs.set_title('Temperature (F)\n UTC timestamp: '+timestamp)
+        axs.set_ylabel('Temp in Degrees F')
+        axs.legend(pdf.columns)
+        axs.grid(True)
+        plt.savefig('static/temp.png')
+
+    def plot_humid_data(self):
+        pdf = self.df[['humid']]
+        fig, axs = plt.subplots(1, 1, figsize=(15,5))
+        axs.plot(pdf, color='lightblue')
+        axs.set_title('Humidity\n UTC timestamp: '+timestamp)
+        axs.set_ylabel('Rel. Humidity (%)')
+        axs.legend(pdf.columns)
+        axs.grid(True)
+        plt.savefig('static/humid.png')
+
+    def plot_light_data(self):
+        pdf = self.df[['light']]
+        fig, axs = plt.subplots(1, 1, figsize=(15,5))
+        axs.plot(pdf, color='orange')
+        axs.set_title('Sunlight\n UTC timestamp: '+timestamp)
+        axs.set_ylabel('Reading from LDR, unsure of unit')
+        axs.legend(pdf.columns)
+        axs.grid(True)
+        plt.savefig('static/light.png')
+
+if __name__ == "__main__":
+    grapher = SensorGrapher()
+    grapher.fetch_df()
+    grapher.plot_moisture_data()    
+    grapher.plot_temp_data()
+    grapher.plot_light_data()
+    grapher.plot_humid_data()    
+    
