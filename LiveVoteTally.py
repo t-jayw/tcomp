ea47af916d48ec96441f95f53115d1bcd86975d0
tyler wood <tylerjaywood@gmail.com>
2019-02-17 17:43:33 -0700

    adding file for live vote tally

---
+++ LiveVoteTally.py
@@ -0,0 +1,76 @@
+import db_connect
+import praw
+import reddit_config as c
+
+from post_templates import continuous_vote_display
+
+cur = db_connect.db_connect()
+
+r = c.getReddit()
+
+### Get most recent water thread
+cur.execute('''
+  SELECT post_id
+  FROM water_thread_id
+  ORDER BY time_stamp DESC
+  LIMIT 1
+  ''')
+thread_id = cur.fetchone()[0]
+s = r.submission(thread_id)
+
+
+tally_sql = """
+SELECT comment_vote
+FROM thread_votes 
+WHERE thread_id = '%s'
+AND comment_vote = %i
+"""
+
+### GET YES
+cur.execute(tally_sql%(thread_id, 1))
+YES = len(cur.fetchall())
+
+### GET NO
+cur.execute(tally_sql%(thread_id, -1))
+NO = len(cur.fetchall())
+
+def make_bars(yes, no):
+	yes_pct = round(yes*100/(yes+no))
+	no_pct = round(100 - yes_pct)
+
+	yes_bar = "`YES`: `"+"|"*(int(yes_pct/2))+"` (%d)"%(yes)
+	no_bar = "`NO`: `"+"|"*(int(no_pct/2))+"` (%d)"%(no)
+
+	return yes_bar, no_bar
+
+def check_for_existing_comment():
+	cur.execute("""SELECT comment_id 
+							FROM live_vote_comment_id 
+							WHERE thread_id = '%s'"""%(thread_id))
+	result = cur.fetchone()
+	if result:
+		result = result[0]
+	print(result)
+	return result
+
+def update_comment():
+	yes, no = make_bars(YES, NO)
+	continuous_score_body = continuous_vote_display%(yes, no)
+	
+	comment_id = check_for_existing_comment()
+	
+	if comment_id:
+		update = r.comment(comment_id)
+		print(update)
+		update.edit(continuous_score_body)
+	else:
+		update = s.reply(continuous_score_body)
+		comment_id = update.id
+		cur.execute('INSERT INTO live_vote_comment_id VALUES(%s, %s)',
+									(thread_id, comment_id)) 
+	
+	update.mod.distinguish(sticky=True)
+
+
+if __name__ == '__main__':
+  update_comment()
